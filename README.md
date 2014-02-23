6441cefd865f185e818e8d7d09701385ef530411
Samir Undavia <samir1@brandeis.edu>
2014-02-22 23:07:00 -0500

    edits for DigitalOcean and added read me

---
+++ README.md
@@ -0,0 +1,9 @@
+<h2>i speak american</h2>
+
+This is an SMS interface for Google Translate.
+
+Texting the phone number returns the message sent in English.
+
+The files are hosted using DigitalOcean.
+
+The text message interface uses Twilio.
--- smstranslate_server.rb
+++ smstranslate_server.rb
@@ -4,28 +4,29 @@ require 'uri'
 require 'twilio-ruby'
 require 'htmlentities'
 
-KEY = ''
+set :bind, '0.0.0.0'
+
+KEY = 'SERVER_KEY'
 
 get '/' do
-	coder = HTMLEntities.new
-	get_translation params['Body']
-	twiml = Twilio::TwiML::Response.new do |r|
-		coder.decode(r.Message get_translation params['Body'])
-	end
-	coder.decode(twiml.text)
+        coder = HTMLEntities.new
+        twiml = Twilio::TwiML::Response.new do |r|
+                coder.decode(r.Message get_translation params['Body'])
+        end
+        coder.decode(twiml.text)
 end
 
 def detect_language q
-	source = open(URI.escape("https://www.googleapis.com/language/translate/v2/detect?key=#{KEY}&q=#{q}")).read
-	start = source.index('"language": "') + '"language": "'.length
-	language = source[start] + source[start+1]
-	language
+        source = open(URI.escape("https://www.googleapis.com/language/translate/v2/detect?key=#{KEY}&q=#{q}")).read
+        start = source.index("\"language\": \"") + ("\"language\": \"").length
+        language = source[start] + source[start+1]
+        language
 end
 
 def get_translation q
-	source = open(URI.escape("https://www.googleapis.com/language/translate/v2?key=#{KEY}&q=#{q}&source=#{detect_language q}&target=en")).read
-	start = source.index('"translatedText": "') + '"translatedText": "'.length
-	text = source[start, start]
-	text = text[0, text.index('"')]
-	text
+        source = open(URI.escape("https://www.googleapis.com/language/translate/v2?key=#{KEY}&q=#{q}&source=#{detect_language q}&target=en")).read
+        start = source.index("\"translatedText\": \"") + ("\"translatedText\": \"").length
+        text = source[start, start]
+        text = text[0, text.index("\"")]
+        text
 end
